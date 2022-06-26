b36303fc9816980fc57eee9652c1810b8bcab6e3
titandog24 <chcoduutn@gmail.com>
2022-06-26 15:23:19 -0600

    Se finaliza la calculadora con React

--- calculadora/src/App.jsx
+++ calculadora/src/App.jsx
@@ -1,41 +1,59 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ButtonList from './components/ButtonList';
 import Function from './components/Function';
 import MathOperations from './components/MathOperations';
 import Result from './components/Result';
-
+import words from 'lodash.words';
 
 import './resources/App.css'
 
-const clickHandler = value => {
-    console.log(value);
+
+const clickHandler = (value, setTexto) => {
+    setTexto(value)
 }
 
-const clickOperations = value => {
-    console.log(value);
-}
-const clickEquals = value => {
-    console.log('valor equal: ' + value);
+const clickEquals = (value, setTexto) => {
+    // eslint-disable-next-line no-eval
+    setTexto(eval(value).toString())
 }
 
-const clickClear = value => {
-    console.log(value);
-}
-const clickR = value => {
-    console.log(value);
+const clickR = (value, setTexto) => {
+    if (value.length > 0) {
+        const newText = value.substring(0, value.length - 1)
+        setTexto(newText)
+    }
 }
 
 const App = () => {
     const valoresDeBotones = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
-
+    const stateApp = useState("")
+    const [texto, setTexto] = stateApp
+    const items = words(texto,/[^-^+^*^/]+/g)
+    const resultado = items.length === 0 ? '0' : items[items.length-1]
+    debugger
     return (
         <main className='react-calculator'>
-            <Result></Result>
+            <Result>{resultado}</Result>
+
             <div className='numbers'>
-                <ButtonList evento={clickHandler} lista={valoresDeBotones}></ButtonList>
+                <ButtonList evento={number => {
+                    clickHandler(texto + number, setTexto)
+                }} lista={valoresDeBotones} />
             </div>
-            <Function onContentClear={clickClear} onDelete={clickR}></Function>
-            <MathOperations onClickOperation={clickOperations} onClickEquals={clickEquals} />
+
+            <Function onContentClear={() => {
+                setTexto('')
+            }}
+                onDelete={() => {
+                    clickR(texto, setTexto)
+                }} />
+
+            <MathOperations onClickOperation={value => {
+                clickHandler(texto + value, setTexto)
+            }}
+                onClickEquals={() => {
+                    clickEquals(texto, setTexto)
+                }} />
         </main>
     );
 }
